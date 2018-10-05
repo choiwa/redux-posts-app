c3fa28c9b61669fd48f3f50013f0f9477b9fcf00
Joyce Cho <joyjoycho@gmail.com>
2018-10-05 14:29:24 -0700

    create Posts.js and Postfrom.js

---
+++ src/components/Posts.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+// create a subclass (child) Posts of class of Component
+class Posts extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Posts </h1>
+      </div>
+    )
+  }
+}
+
+export default Posts;
