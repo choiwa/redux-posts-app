884f74fe58bf6317a0affb28a0c6653a9a0b8506
Joyce Cho <joyjoycho@gmail.com>
2018-10-05 15:36:43 -0700

    posts render

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,10 +2,35 @@ import React, {Component} from 'react';
 
 // create a subclass (child) Posts of class of Component
 class Posts extends Component {
+  // The constructor method is a special method for creating and initializing an object created with a class
+  constructor(props) {
+    // The super keyword is used to call corresponding methods of super class. This is one advantage over prototype-based inheritance.
+    super(props);
+
+    this.state = {
+      posts: [],
+    }
+  }
+  componentDidMount() {
+    // https://jsonplaceholder.typicode.com/ for fake data
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    // .json() returns a promise that resolves with the result of parsing the body text as JSON
+    .then(response => response.json())
+    .then(data => this.setState({posts: data}));
+  }
+
   render() {
+    // map the posts here because calling function outside the render will not change statea and will not render
+    const postItems = this.state.posts.map(post =>
+      <div key={post.id}>
+        <h3>{post.title}</h3>
+        <p>{post.body}</p>
+      </div>);
+      
     return (
       <div>
         <h1> Posts </h1>
+        {postItems}
       </div>
     )
   }
