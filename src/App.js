3176b9a8a25dffcf393edb228840e096602770c5
Joyce Cho <joyjoycho@gmail.com>
2018-10-05 17:00:21 -0700

    add redux, redux-thunk, and react-redux. Add store to App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 import Posts from './components/Posts';
 import Postform from './components/Postform';
+// import Provider from react-redux
+import { Provider } from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
 
+// about Provider
+// https://redux.js.org/basics/usagewithreact
+
+// about store
+// https://github.com/reduxjs/redux/blob/master/docs/api/Store.md
+
+// create Store
+// https://github.com/reduxjs/redux/blob/master/docs/api/createStore.md
+const store = createStore(()=> [], {}, applyMiddleware());
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Postform />
-        <hr />
-        <Posts />
-      </div> 
+      <Provider store={store}>
+        <div className="App">
+          <Postform />
+          <hr />
+          <Posts />
+        </div> 
+      </Provider>
     );
   }
 }
