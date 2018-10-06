7583d77e5eafbfa19849d9e6bcb37de7236b9f2f
Joyce Cho <joyjoycho@gmail.com>
2018-10-05 18:31:18 -0700

    implemented redux dev tools

--- src/store.js
+++ src/store.js
@@ -4,12 +4,20 @@
 // create Store
 // https://github.com/reduxjs/redux/blob/master/docs/api/createStore.md
 
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers'
 
 const initialState = {};
 const middleware = [thunk];
-const store = createStore(rootReducer, initialState, applyMiddleware(...middleware));
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+
+);
 
 export default store;
